e9ddc9efea5063bab9a45b979815b315fb4db9a9
Stephen Szpak <stephen.szpak@ingramcontent.com>
2019-01-11 14:33:06 -0600

    added fetch blob and image picker

--- android/app/src/main/java/com/calcoun/MainApplication.java
+++ android/app/src/main/java/com/calcoun/MainApplication.java
@@ -3,6 +3,8 @@ package com.calcoun;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
+            new RNFetchBlobPackage(),
             new VectorIconsPackage(),
             new RNFirebasePackage(),
             new RNGestureHandlerPackage()
